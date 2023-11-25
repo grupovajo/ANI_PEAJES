7dcb96eec7af77cce3348fbd49d2cacaf3d4c3b0
johnrofa <johnrofa@hotmail.com>
2023-11-25 11:09:09 -0500

    Actualización

--- Tablero/Proyecto Django & Plotly/fin_manager/urls.py
+++
@@ -1,9 +0,0 @@
-#from django.conf.urls import url
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('dashboard1', views.dashboard1, name='dashboard1'),
-    path('accounts/register/', views.register, name='register'),
-]
